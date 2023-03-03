ee35307dc896a1543550efbef97fba64fec35f14
Michael Gilbert Hansen <60772847+Gilbert40aar@users.noreply.github.com>
2023-03-03 11:59:04 +0100

    done

---
+++ HansensTodo/Migrations/20230303073822_todo2.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HansensTodo.Migrations
+{
+    /// <inheritdoc />
+    public partial class todo2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoHash",
+                columns: table => new
+                {
+                    HashId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserEmailId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HashedData = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Dato = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoHash", x => x.HashId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Todos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserEmailId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TodoTitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StackSize = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoHash");
+
+            migrationBuilder.DropTable(
+                name: "Todos");
+        }
+    }
+}
